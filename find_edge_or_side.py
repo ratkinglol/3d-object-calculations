f54ac1cab27bbc158ca07b449fd2b797b307071b
ratkinglol <131613218+ratkinglol@users.noreply.github.com>
2024-04-05 22:45:56 -0400

    Create find_edge_or_side.py

---
+++ find_edge_or_side.py
@@ -0,0 +1,14 @@
+import math
+
+def rectangular_prism_find_length(width, height, volume):
+  rectangular_prism_find_length_final = volume / (width * height)
+  return rectangular_prism_find_length_final
+
+def rectangular_prism_find_width(length, height, volume):
+  rectangular_prism_find_width_final = volume / (length * height)
+  return rectangular_prism_find_width_final
+
+def rectangular_prism_find_height(length, width, volume):
+  rectangular_prism_find_height_final = volume / (length * width)
+  return rectangular_prism_find_height_final
+
